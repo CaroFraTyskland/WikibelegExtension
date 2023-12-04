e0308ed7c97da800024f52a4fd4c9f39435273b0
CaroFraTyskland <bruckmeier@fim.uni-passau.de>
2023-12-04 14:27:48 +0100

    new urls

--- background.js
+++ background.js
@@ -22,7 +22,7 @@ chrome.action.onClicked.addListener((tab) => {
     chrome.scripting.executeScript({
       target: { tabId: tab.id },
       function: (url, dateString) => {
-         title = document.getElementsByClassName("page-title__heading-text")[0].innerText;
+         title = document.getElementsByClassName("page-title")[0].innerText;
 
          let authors = document.getElementsByClassName("article-info__author-list");
          let author = authors.length > 0 ? authors[0].innerText : "";
@@ -36,9 +36,9 @@ chrome.action.onClicked.addListener((tab) => {
     chrome.scripting.executeScript({
       target: { tabId: tab.id },
       function: (url, dateString) => {
-         title = document.getElementsByTagName("h2")[0].innerText;
+         title = document.getElementById('main-content').getElementsByTagName("h2")[0].innerText;
 
-         ref = "<ref>{{Internetquelle |url=" + url + " |titel=" + title + " |abruf=" + dateString + " |werk=NVE  |sprache=no}}</ref>";    
+         ref = "<ref>{{Internetquelle |url=" + url + " |titel=" + title + " |abruf=" + dateString + " |werk=NVE |sprache=no}}</ref>";    
          alert(ref); 
       },
       args: [url, dateString]
@@ -47,7 +47,7 @@ chrome.action.onClicked.addListener((tab) => {
     chrome.scripting.executeScript({
       target: { tabId: tab.id },
       function: (url, dateString) => {
-         title = document.getElementsByTagName("h1")[1].innerText;
+         title = document.getElementsByTagName("h1")[0].innerText;
 
          ref = "<ref>{{Internetquelle |url=" + url + " |titel=" + title + " |abruf=" + dateString + " |werk=Bane Nor |sprache=no}}</ref>";    
          alert(ref); 
@@ -65,6 +65,37 @@ chrome.action.onClicked.addListener((tab) => {
       },
       args: [url, dateString]
     });
+  } else if (url.includes('stortinget.no/no/Representanter-og-komiteer/Representantene/Representant')) {
+    chrome.scripting.executeScript({
+      target: { tabId: tab.id },
+      function: (url, dateString) => {
+        title = document.title
+
+        ref = "<ref>{{Internetquelle |url=" + url + " |titel=" + title + " |abruf=" + dateString + " |werk=Stortinget |sprache=no}}</ref>";    
+        alert(ref); 
+      },
+      args: [url, dateString]
+    });
+  } else if (url.includes('kart.ssb.no')) {
+    chrome.scripting.executeScript({
+      target: {tabId: tab.id },
+      function: (url, dateString) => {
+         ref = "<ref>{{Internetquelle |url=https://kart.ssb.no/ |titel=Kart fra Statistisk sentralbyrå |abruf= " + dateString + " |werk=ssb.no |hrsg=Statistisk sentralbyrå |sprache=no}}</ref>"
+
+         alert(ref); 
+      },
+      args: [url, dateString]
+    });
+  } else if (url == "https://www.regjeringen.no/no/tema/kommuner-og-regioner/kommunestruktur/nye-kommune-og-fylkesnummer-fra-1.-januar-2024/id2924701/") {
+      chrome.scripting.executeScript({
+        target: {tabId: tab.id },
+        function: (url, dateString) => {
+          ref = "<ref>{{Internetquelle |url=https://www.regjeringen.no/no/tema/kommuner-og-regioner/kommunestruktur/nye-kommune-og-fylkesnummer-fra-1.-januar-2024/id2924701/ |titel=Nye fylkes- og kommunenummer fra 2024 |werk=regjeringen.no |datum=2022-08-23 |sprache=no |abruf="+ dateString +"}}</ref>"
+
+          alert(ref); 
+        },
+        args: [url, dateString]
+      });  
   }
 
 });
